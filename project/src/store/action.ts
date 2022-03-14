df2504143397dc534508451b07ca5cfd2dbc2e41
Ksenia <kpuchkova@gmail.com>
2022-03-14 10:27:27 +0300

    Добавлена сортировка

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -3,7 +3,6 @@ import {createAction} from '@reduxjs/toolkit';
 export enum Action {
   CHANGE_CITY = 'Change_City',
   CHANGE_SORT_TYPE = 'Change_Sort_Type',
-  RESET_SORT = 'Reset_Sort'
 }
 
 export const changeCity = createAction<string>(Action.CHANGE_CITY);
